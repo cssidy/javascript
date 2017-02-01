f3943848c3bdc64881a0413636359f768bbd07a8
cssidy <cassidy@cassidybrooke.net>
2017-02-01 16:26:39 -0500

    simple syntax

---
+++ Javascript - The Definitive Guide/syntax.js
@@ -0,0 +1,28 @@
+/**
+ * Created by cassidy on 2/1/17.
+ */
+
+var book = {
+    topic: "JavaScript",
+    fat: true
+};
+
+// book.topic
+// book["fat"]
+book.author = "Flanagan";
+book.contents = {};
+
+var primes = [2, 3, 5, 7];
+// primes[0]
+primes[4] = 9;
+
+
+var points = [
+    {x:0, y:0},
+    {x:1, y:1}
+];
+
+var data = {
+    trail1: [[1,2], [3,4]],
+    trial2: [[2,3], [4,5]]
+}
