648c70b6dd6319342610c720357a1ab8b0ba9b90
cssidy <cassidy@cassidybrooke.net>
2018-01-16 12:07:07 -0500

    starting to work on conditional statement exercises

--- Eloquent Javascript/chapter-2.js
+++ Eloquent Javascript/chapter-2.js
@@ -1,3 +1,48 @@
 /**
  * Created by cassidy on 1/16/18.
  */
+
+var ten = 10;
+console.log(ten * ten);
+// 100
+
+var mood = "light";
+console.log(mood);
+// light
+
+mood = "dark";
+console.log(mood);
+// dark
+
+var luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+// 105
+
+var one = 1, two = 2;
+console.log(one + two);
+// 3
+
+alert("Good morning!");
+
+var x = 30;
+console.log("the value of x is: ", x);
+// the value of x is: 30
+
+console.log(Math.max(2, 4));
+// 4
+
+console.log(Math.min(2, 4) + 100);
+// 102
+
+confirm("Shall we, then?");
+
+prompt("Tell me everything you know.", "...");
+
+var theNumber = Number(prompt("Pick a number.", ""));
+alert("Your number is the square root of " + theNumber * theNumber);
+
+var theNumber = prompt("Pick a number.", ""));
+if (!isNaN(theNumber))
+    alert("Your number is the square root of " + theNumber * theNumber);
+
