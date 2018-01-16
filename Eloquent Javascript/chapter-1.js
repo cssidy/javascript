b20847f20d34044b841073d5f662fe5470e8c755
cssidy <cassidy@cassidybrooke.net>
2018-01-16 11:24:10 -0500

    change file types

--- Eloquent Javascript/chapter-1.js
+++ Eloquent Javascript/chapter-1.js
@@ -1,3 +1,32 @@
 console.log(typeof 4.5);
+// number
+console.log(typeof "x");
+// string
+console.log(-(10-2));
+// -8
+console.log(3 > 2);
+// true
+console.log(3 < 2);
+// false
+console.log("Aardvark" < "Zoroaster");
+// true
+console.log("Itchy" != "Scratchy");
+// true
+console.log(NaN == NaN);
+// false
+console.log(true && false);
+// false
+console.log(true && true);
+// true
+console.log(false || true);
+// true
+console.log(false || false);
+// false
+console.log(!false);
+// true
+console.log(!true);
+// false
+console.log(1 + 1 == 2 && 10 * 10 > 50);
+// true
 
 
