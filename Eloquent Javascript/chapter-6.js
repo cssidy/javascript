d400bd14f0772d540de24ca14583972d8e2295b9
cssidy <cassidy@cassidybrooke.net>
2018-02-15 18:01:01 -0500

    finished chap 6, took 1.5 hours

--- Eloquent Javascript/chapter-6.js
+++ Eloquent Javascript/chapter-6.js
@@ -74,5 +74,19 @@ killerRabbit.dance();
 // pass null as the specified prototype to create fresh object with no prototype
 var pupper = Object.create(null);
 
+// we can specify properties that look like normal properties but secretly have methods associated with them
+var pile = {
+    elements: ["eggshell", "orange peel", "worm"],
+    get height() {
+        return this.elements.length;
+    },
+    set height(value) {
+        console.log("Ignoring attempt to set height to", value);
+    }
+};
+console.log(pile.height);
+// 3
+pile.height = 100;
+// Ignoring attempt to set height to 100
 
 
