9fb979924687a1dcbd57782928737e06c00ddebc
cssidy <cassidy@cassidybrooke.net>
2018-02-23 13:49:46 -0500

    solution

--- Eloquent Javascript/electronic_life/code/elife.js
+++ Eloquent Javascript/electronic_life/code/elife.js
@@ -326,6 +326,35 @@ Tiger.prototype.act = function(view) {
     return {type: "move", direction: space};
 };
 
+function SmartTiger() {
+  this.energy = 100;
+  this.direction = "w";
+  // Used to track the amount of prey seen per turn in the last six turns
+  this.preySeen = [];
+}
+SmartTiger.prototype.act = function(view) {
+  // Average number of prey seen per turn
+  var seenPerTurn = this.preySeen.reduce(function(a, b) {
+    return a + b;
+  }, 0) / this.preySeen.length;
+  var prey = view.findAll("O");
+  this.preySeen.push(prey.length);
+  // Drop the first element from the array when it is longer than 6
+  if (this.preySeen.length > 6)
+    this.preySeen.shift();
+
+  // Only eat if the predator saw more than ¼ prey animal per turn
+  if (prey.length && seenPerTurn > 0.25)
+    return {type: "eat", direction: randomElement(prey)};
+
+  var space = view.find(" ");
+  if (this.energy > 400 && space)
+    return {type: "reproduce", direction: space};
+  if (view.look(this.direction) != " " && space)
+    this.direction = space;
+  return {type: "move", direction: this.direction};
+};
+
 var jungle = new LifelikeWorld(
     ["####################################################",
    "#                 ####         ****              ###",
@@ -346,7 +375,7 @@ var jungle = new LifelikeWorld(
    "###               #   *****                    ****#",
    "####################################################"],
   {"#": Wall,
-   "@": Tiger,
+   "@": SmartTiger,
    "O": SmartPlantEater,
    "*": Plant}
 );
