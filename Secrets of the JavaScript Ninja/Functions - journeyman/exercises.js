c33e108949e03d5028f8a12474677659474a8e2e
Cassidy Brooke <cassidy@cassidybrooke.net>
2018-10-27 22:03:30 -0400

    merge

---
+++ Secrets of the JavaScript Ninja/Functions - journeyman/exercises.js
@@ -0,0 +1,15 @@
+// 1
+// Have to write assert function
+/*
+function sum() {
+    var sum = 0;
+    for(var i=0; i<arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+assert(sum(1,2,3) === 6, 'Sum of first three numbers is 6');
+assert(sum(1,2,3,4) === 10, 'Sum of first four numbers is 10');
+*/
+
